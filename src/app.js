519709349c8925deab4f7d43af26fcc173310fcd
Perminder <kaurperminder47@gmail.com>
2025-10-13 16:53:13 +0530

    validators

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ const User = require("./models/user")
 
 app.use(express.json());
 
+
+
+//posting date to database
+
 app.post("/signup", async (req, res) => {
 
     console.log(req.body)
 
     const user = new User(req.body);
-
-
 
 
     try {
@@ -35,22 +37,29 @@ app.post("/signup", async (req, res) => {
 
 });
 
-
-
+//getting data form databse
 //Get user by eamil
+
+
 
 
 app.get("/user", async (req, res) => {
 
-    const userEmail = req.body.eamilId;
+    const userEmail = req.body.emailId;
 
 
 
     try {
 
         const user = await User.find({ emailId: userEmail });
+        if (user.length === 0) {
 
-        res.send(user);
+            res.status(404).send("User is not found");
+        } else {
+            res.send(user);
+
+        }
+
     }
     catch (err) {
 
@@ -59,13 +68,92 @@ app.get("/user", async (req, res) => {
 
 });
 
-//Feed API-GET/fedd-get all the users from the database
 
-app.get("/feed", (req, res) => {
 
+
+//Feed API-GET/feed-get all the users from the database
+
+app.get("/feed", async (req, res) => {
+
+    try {
+
+        const users = await User.find({});
+        res.send(users);
+
+    } catch (error) {
+
+
+        res.status(400).send("something went wrong");
+
+    }
 
 
 });
+
+//this api to delete user form database
+
+app.delete("/user", async (req, res) => {
+
+    const userId = req.body.userId;
+
+    try {
+
+
+        const user = await User.findByIdAndDelete(userId);
+
+        res.send("user deleted successfully");
+    }
+    catch (err) {
+
+        res.status(400).send("something went wrong");
+
+
+
+    }
+});
+
+
+//this api update the data of user in dattabase
+
+app.patch("/user/:userId", async (req, res) => {
+
+    const userId = req.params?.userId;
+
+    const data = req.body;
+
+
+
+    try {
+
+
+        const ALLOWED_UPDATES = [ "photoUrl", "about", "gender", "age", "skills"];
+
+
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+
+        if (!isUpdateAllowed) {
+
+            throw new Error("update not allowed");
+        }
+
+        if(data?.skills.length>10){
+
+            throw new Error("Skills cannot be more than 10")
+        };
+
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after", runValidators: true });
+
+        console.log(user);
+
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("UPDATE FAILED:" + err.message);
+    }
+
+
+})
+
 
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,34 +1,98 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+const validator =require("validator");
 
 const userSchema = new mongoose.Schema({
 
-    firstName:{
+    firstName: {
 
-        type:String
+        type: String,
+        required: true,
     },
 
-    lastName:{
+    lastName: {
 
-        type:String
+        type: String
     },
 
-    password:{
+    emailId: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
 
-        type:String
+            if(!validator.isEmail(value)){
+
+                throw new Error("Invalid enalid address"+value);
+            }
+        },
     },
 
-    age:{
+    password: {
 
-        type:Number
+        type: String,
+        required: true,
+        validate(value){
+
+            if(!validator.isStrongPassword(value)){
+
+                throw new Error("Enter a strong password:"+value)
+            }
+        }
+
+
     },
 
-    gender:{
+    age: {
 
-        type:String
-    }
+        type: Number,
+        min: 18
 
+    },
+
+    gender: {
+
+        type: String,
+        validate(value) {
+
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gendewr data is not valid")
+
+
+            }
+        }
+    },
+    photoUrl: {
+
+        type: String,
+        default: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_incoming&w=740&q=80",
+        validate(value){
+            if(!validator.isURL(value)){
+
+                throw new Error("Invalid url:"+value);
+            }
+        },
+    },
+
+    about: {
+
+        type: String,
+        default: "This is a default about of the user",
+    },
+    skills: {
+
+        type: [String]
+    },
+
+   
+
+
+},{
+
+    timestamps:true,
 });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports=User;
+module.exports = User;
